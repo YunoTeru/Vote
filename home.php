6955964e43ddb62eb9b8bf68d199dd4bdae44100
Yuno <yunoshin0417@icloud.com>
2019-05-07 17:11:08 +0800

    Recent

---
+++ home.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>HOME</title>
+    <style type="text/css"> 
+    ul.topnav {
+        overflow: hidden;
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+        background-color: #5B5B5B;
+    }
+    ul.topnav li {
+        float: left;
+    }
+    ul.topnav li a {
+        display: block;
+        padding: 14px 16px;
+        text-align: center;
+        text-decoration: none;
+        color: white;
+    }
+    ul.topnav li a:hover:not(.active) {
+        background-color: #a9bce2;
+    }
+    ul.topnav li a.active {
+        background-color: #da3c41;
+    }
+
+    @media screen and (max-width: 480px) {
+        ul.topnav li.right, ul.topnav li {
+            float: none;
+        }
+    }
+    .search{
+        margin-top: 8px;
+        position: relative;
+        left: 900px;
+    }
+    .btn{
+        background-color: #BBBB;
+       
+        color: #fff
+        
+    }
+    
+    .btn:hover {
+        background-color: gray;
+    }
+
+
+    .box{
+        position: relative;
+        float: right;
+        width: 25%;
+        height: auto;
+    }
+
+    .img{
+        width: 250px;
+        height: auto;
+       margin-left: 50px;
+       margin-top: 50px;
+    }
+
+    </style>
+</head>
+<body>
+<ul class="topnav">
+	<li><a href="home.php">Home</a></li>
+    <li><a href="popular.php">Popular</a></li>
+    <li><a href="upload.php">Upload</a></li>
+    <li class="search">
+        <input type="text" class="form-controle ">
+        <button class="btn text-white">Search<i class="fa fa-search"></i></button>
+    </li>
+</ul>
+<ul></ul>
+    <div class="box">
+        <img class="img" src="images/img1.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img2.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img3.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img9.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img5.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img6.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img7.jpg" alt="">
+    </div>
+    <div class="box">
+        <img class="img" src="images/img8.jpg" alt="">
+    </div>
+</body>
+</html>
